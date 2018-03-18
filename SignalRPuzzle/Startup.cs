39a46bf7f75e59f805b6d050e1996e5e66883599
Tom Witecki <witeckitom@gmail.com>
2018-03-18 10:36:16 -0500

    Modify the katana startup to utilize the new project structure

--- SignalRPuzzle/Startup.cs
+++ SignalRPuzzle/Startup.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
 using Owin;
 
 [assembly: OwinStartup(typeof(SignalRPuzzle.Startup))]
@@ -10,6 +12,16 @@ namespace SignalRPuzzle
         {
             // Any connection or hub wire up and configuration should go here
             app.MapSignalR();
+
+            const string clientAppFolder = "ClientApp";
+            var fileSystem = new PhysicalFileSystem(clientAppFolder);;
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                FileSystem = fileSystem
+            };
+            app.UseFileServer(options);
+
         }
     }
 }
